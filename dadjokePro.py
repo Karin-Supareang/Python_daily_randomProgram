96558bb8efe5459f00a987db1e47c5e495c4f479
Karin-Supareang <karin.sup@dome.tu.ac.th>
2024-12-23 19:50:36 +0700

    slight typo on line 17

--- dadjokePro.py
+++ dadjokePro.py
@@ -1,7 +1,7 @@
 # try using: random lib, random(), list , index(), and \"Quotationmark\" 
 import random
 
-joke =[ "I went to the aquarium this weekend, but I didn’t stay long.", "Why can’t orphans play baseball?", "I visited my new friend in his apartment. He told me to make myself at home.", "Do you know the phrase \"One man's trash is another man's treasure\"?", "Give a man a match, and he'll be warm for a few hours."]
+joke = [ "I went to the aquarium this weekend, but I didn’t stay long.", "Why can’t orphans play baseball?", "I visited my new friend in his apartment. He told me to make myself at home.", "Do you know the phrase \"One man's trash is another man's treasure\"?", "Give a man a match, and he'll be warm for a few hours."]
 punchline = [ "There’s something fishy about that place.", "They don’t know where home is.", "So I threw him out. I hate having visitors.", "It's a wonderful saying but a horrible way to find out that you were adopted.", "Set him on fire, and he will be warm for the rest of his life."]
 randomNoise = ["Duh", "womp womp", "Bruh", "Nihaha", "Huh", "Lol", "Wheez"]
 
@@ -14,7 +14,7 @@ while not user_answer == "no":
     punchlineIndex = joke.index(tellJoke)
     make_noise = random.choice(randomNoise)
     user_answer = user_answer.lower()
-    print(f"Ok, here some joke. {tellJoke}")
+    print(f"Ok, here's some joke. {tellJoke}")
     user_answer = input("your respond (can escape): ")
     print(f"{punchline[punchlineIndex]} {make_noise}")
     user_answer = input("Wanna hear some more jokes? (type no to excape): ")
